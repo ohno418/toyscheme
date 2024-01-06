afe36690ee7076dd36ccc2f82163f20479a266c4
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-01-06 23:41:38 +0900

    read-eval-print loop
    Just read user input and print it as is, for now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        stdout().flush().expect("stdout flush failed");
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("read_line() failed");
+
+        print!("{input}");
+    }
 }
