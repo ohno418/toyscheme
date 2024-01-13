9098bfbef8434f2aa5a39fb0776ad6df8f0d239a
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-01-13 16:02:03 +0900

    Add some tests for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,29 @@ pub fn eval(input: &str) -> EvalResult {
     let ast = parser::parse_expr(input);
     eval::eval(ast)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn eval_empty_input() {
+        let input = "\n";
+        let result = eval(input);
+        assert_eq!(result.to_string(), "");
+    }
+
+    #[test]
+    fn eval_number() {
+        let input = "42\n";
+        let result = eval(input);
+        assert_eq!(result.to_string(), "42");
+    }
+
+    #[test]
+    fn eval_unknown_ident() {
+        let input = "hi\n";
+        let result = eval(input);
+        assert_eq!(result.to_string(), "error: unknown input");
+    }
+}
