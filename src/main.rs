5499f07131b70720b013c175d19ff84b9e5cc641
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-01-13 14:07:21 +0900

    chore

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 fn read() -> String {
     print!("> ");
     stdout().flush().expect("stdout flush failed");
+
     let mut input = String::new();
     stdin().read_line(&mut input).expect("read_line() failed");
     input
